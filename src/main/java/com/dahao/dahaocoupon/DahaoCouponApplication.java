711cdceba76e1bb7cb33d8d799fd9d80ce607943
jinbiao <jinbiao@jinbiao.com>
2020-12-02 13:30:47 +0800

    框架

---
+++ src/main/java/com/dahao/dahaocoupon/DahaoCouponApplication.java
@@ -0,0 +1,13 @@
+package com.dahao.dahaocoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DahaoCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DahaoCouponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dahao/dahaocoupon/DahaoCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dahao.dahaocoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DahaoCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
